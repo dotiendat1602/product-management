2051ef4611eee84a1dc89c0dab37872f795fcb66
Do Tien Dat <dotiendat711@gmail.com>
2024-07-05 10:35:21 +0700

    Tạo folder Controller

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("client/pages/products/index");
-})
+const controller = require("../../controllers/client/product.controller");
+
+router.get("/", controller.index);
 
 // Ví dụ về việc sau này có thêm nhiều trang con khác nữa
-// router.post("/create", (req, res) => {
-//     res.render("client/pages/products/index");
-// })
+// router.post("/create", controller.create);
 
-// router.patch("/edit", (req, res) => {
-//     res.render("client/pages/products/index");
-// })
+// router.patch("/edit", controller.edit);
 
-// router.get("/detail", (req, res) => {
-//     res.render("client/pages/products/index");
-// })
+// router.get("/detail", controller.detail);
 
 module.exports = router;
