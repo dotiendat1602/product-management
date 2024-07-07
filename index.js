16000303f390f144a3fc58ea27132b9daf3cc3e4
Do Tien Dat <dotiendat711@gmail.com>
2024-07-07 21:35:39 +0700

    Tối ưu lại biến admin

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ database.connect();
 
 const routeAdmin = require("./routes/admin/index.route.js");
 const routeClient = require("./routes/client/index.route.js");
+const systemConfig = require("./config/system.js");
+const system = require('./config/system.js');
 
 const app = express();
 const port = process.env.PORT;
@@ -14,6 +16,9 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static('public'));
+
+// App local Variables (chỉ dùng được trong các file pug)
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 routeAdmin.index(app);
 routeClient.index(app);
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,10 @@
 const dashboardRoute = require("./dashboard.route");
 const productsRoute = require("./product.route");
+const systemConfig = require("../../config/system");
 
 // Cú pháp để có thể export được hàm có tên là index sang các file khác
 module.exports.index = (app) => {
-    app.use('/admin/dashboard', dashboardRoute);
-    app.use('/admin/products', productsRoute);
+    const path = `/${systemConfig.prefixAdmin}`;
+    app.use(`${path}/dashboard`, dashboardRoute);
+    app.use(`${path}/products`, productsRoute);
 }
