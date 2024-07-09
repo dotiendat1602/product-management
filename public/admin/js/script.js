8a922dfe1c1f56001b1745d2918a8b1c3e2f7e39
Do Tien Dat <dotiendat711@gmail.com>
2024-07-09 18:12:55 +0700

    Tìm kiếm theo nhiều tiêu chí

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -24,3 +24,23 @@ if(listButtonStatus.length > 0){
     }
 }
 // End Button Status
+
+// Form search
+const formSearch = document.querySelector("[form-search]");
+if(formSearch){
+    formSearch.addEventListener("submit", (event) => {
+        let url = new URL(window.location.href);
+
+        // Hàm này là hàm ngăn chặn hành vi mặc định của 1 cái gì đó
+        // ở đây hành vi mặc định của form là load lại trang web nên hàm này sẽ ngăn điều đó khi submit
+        event.preventDefault();
+        const keyword = event.target.elements.keyword.value;
+        if(keyword){
+            url.searchParams.set("keyword", keyword);
+        } else{
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    })
+}
+// End Form search
