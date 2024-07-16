39f41c92ac5ab5cb93e813b80f00e3d0a52aecf2
Do Tien Dat <dotiendat711@gmail.com>
2024-07-16 21:17:27 +0700

    Tính năng tạo mới sản phẩm - Thêm trường createdAt, updatedAt và slug tự động sinh ra

--- model/product.model.js
+++ model/product.model.js
@@ -1,22 +1,30 @@
 const { default: mongoose } = require("mongoose");
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
-const Product = mongoose.model(
-    'Product', 
-    {
-        title: String,
-        description: String,
-        price: Number,
-        discountPercentage: Number,
-        stock: Number,
-        thumbnail: String,
-        status: String,
-        position: Number,
-        deleted: {
-            type: Boolean,
-            default: false
-        }
+const productSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: {
+        type: Boolean,
+        default: false
     },
-    "products"
-);
+    slug: {
+        type: String,
+        slug: "title",
+        unique: true
+    }
+}, {
+    timestamps: true 
+    // Tự động thêm trường createdAt và updatedAt (https://mongoosejs.com/docs/timestamps.html)
+});
+
+const Product = mongoose.model('Product', productSchema, "products");
 
 module.exports = Product;
