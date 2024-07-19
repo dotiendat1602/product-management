98c6edc852717d041fe8b2feb1f2682d8fde0a50
Do Tien Dat <dotiendat711@gmail.com>
2024-07-19 19:04:35 +0700

    Làm trang chi tiết sản phẩm bên client

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,11 +5,6 @@ const controller = require("../../controllers/client/product.controller");
 
 router.get("/", controller.index);
 
-// Ví dụ về việc sau này có thêm nhiều trang con khác nữa
-// router.post("/create", controller.create);
-
-// router.patch("/edit", controller.edit);
-
-// router.get("/detail", controller.detail);
+router.get("/:slug", controller.detail);
 
 module.exports = router;
