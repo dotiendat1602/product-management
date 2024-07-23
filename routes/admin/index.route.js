9acdd100ecd39aece2bb0d05f18611bb8e561f99
Do Tien Dat <dotiendat711@gmail.com>
2024-07-23 18:46:28 +0700

    Xây dựng phần quản lý danh mục sản phẩm (Khung)

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,6 @@
 const dashboardRoute = require("./dashboard.route");
 const productsRoute = require("./product.route");
+const productsCategoryRoute = require("./product-category.route");
 const systemConfig = require("../../config/system");
 
 // Cú pháp để có thể export được hàm có tên là index sang các file khác
@@ -7,4 +8,5 @@ module.exports.index = (app) => {
     const path = `/${systemConfig.prefixAdmin}`;
     app.use(`${path}/dashboard`, dashboardRoute);
     app.use(`${path}/products`, productsRoute);
+    app.use(`${path}/products-category`, productsCategoryRoute);
 }
