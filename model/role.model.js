3fda91f867dfd375048394a5b7751f3583faeed0
Do Tien Dat <dotiendat711@gmail.com>
2024-07-27 19:43:18 +0700

    Tạo collection roles

---
+++ model/role.model.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose");
+
+const roleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permissions: {
+        type: Array,
+        default: []
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true 
+    // Tự động thêm trường createdAt và updatedAt (https://mongoosejs.com/docs/timestamps.html)
+});
+
+const Role = mongoose.model('Role', RoleSchema, "roles");
+
+module.exports = Role;
