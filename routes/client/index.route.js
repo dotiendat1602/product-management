c13c54a00a8825ffbabe8540b680d785069211ac
Do Tien Dat <dotiendat711@gmail.com>
2024-08-05 20:52:59 +0700

    Thêm sản phẩm vào giỏ hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,16 +1,22 @@
 const homeRoute = require("./home.route");
 const productRoute = require("./product.route");
 const searchRoute = require("./search.route");
+const cartRoute = require("./cart.route");
+
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const cartMiddleware = require("../../middlewares/client/cart.middleware");
 
 // Cú pháp để có thể export được hàm có tên là index sang các file khác
 module.exports.index = (app) => {
     app.use(categoryMiddleware.category);
+    app.use(cartMiddleware.cartId);
 
     app.use('/', homeRoute);
     
     app.use('/products', productRoute);
 
     app.use('/search', searchRoute);
+
+    app.use("/cart", cartRoute);
 }
