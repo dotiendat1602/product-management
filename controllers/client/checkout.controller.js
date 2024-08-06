f926bbaed3a524bf7161fd0fa79b9f12448d78bc
Do Tien Dat <dotiendat711@gmail.com>
2024-08-06 20:47:36 +0700

    Lấy data được gửi lên lưu vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,4 +1,5 @@
 const Cart = require("../../model/cart.model");
+const Order = require("../../model/order.model");
 const Product = require("../../model/product.model");
 
 // [GET] /checkout
@@ -30,3 +31,42 @@ module.exports.index = async (req, res) => {
         cartDetail: cart
     });
 }
+
+// [POST] /checkout/order
+module.exports.orderPost = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    });
+
+    const orderData = {
+        userInfo: userInfo,
+        products: []
+    }
+
+    for (const item of cart.products) {
+        const productInfo = await Product.findOne({
+            _id: item.productId
+        });
+
+        orderData.products.push({
+            productId: item.productId,
+            price: productInfo.price,
+            discountPercentage: productInfo.discountPercentage,
+            quantity: item.quantity
+        });
+    }
+
+    const newOrder = new Order(orderData);
+    await newOrder.save();
+
+    await Cart.updateOne({
+        _id: cartId
+    }, {
+        products: []
+    });
+
+    res.redirect(`/checkout/success/${newOrder.id}`);
+}   
