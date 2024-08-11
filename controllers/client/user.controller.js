9410391a7c0e9a6fe3eacd2fe1f8e988732d8b37
Do Tien Dat <dotiendat711@gmail.com>
2024-08-11 20:06:39 +0700

    Quên mật khẩu - Hướng dẫn gửi OTP qua email tự động

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -4,6 +4,7 @@ const User = require("../../model/user.model");
 const ForgotPassword = require("../../model/forgot-password.model");
 
 const generateHelper = require("../../helper/generate.helper");
+const sendEmailHelper = require("../../helper/sendEmail.helper");
 
 // [GET] /user/register
 module.exports.register = async (req, res) => {
@@ -119,7 +120,12 @@ module.exports.forgotPasswordPost = async (req, res) => {
   const forgotPassword = new ForgotPassword(ForgotPasswordData);
   await forgotPassword.save();
 
-  // Việc 2: Gửi mã OTP qua email của user (Tạm thời coi như xong, làm sau)
+  // Việc 2: Gửi mã OTP qua email của user
+  const subject = "Mã OTP lấy lại mật khẩu.";
+  const htmlSendMail = `Mã OTP xác thực của bạn là <b style="color: green;">${otp}</b>. Mã OTP có hiệu lực trong 3 phút. Vui lòng không cung cấp mã OTP cho người khác.`;
+
+  sendEmailHelper.sendEmail(email, subject, htmlSendMail);
+
 
   res.redirect(`/user/password/otp?email=${email}`);  
 };
@@ -169,7 +175,7 @@ module.exports.resetPassword = async (req, res) => {
 // [PATCH] /user/password/reset
 module.exports.resetPasswordPatch = async (req, res) => {
   const password = req.body.password;
-  const tokenUser = req.cookie.tokenUser;
+  const tokenUser = req.cookies.tokenUser;
 
   await User.updateOne({
     tokenUser: tokenUser,
